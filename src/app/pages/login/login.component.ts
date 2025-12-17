8fb647a4350778e1525174c6f3e6f7e3ca903be4
Emily Fogaca <emilyrodrigues17@hotmail.com>
2025-12-17 09:43:40 -0400

    add layout tela de login e authenticacao fake

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,20 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Auth } from '../../services/auth/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-    dadosLogin = {
-      email: '',
-      senha: ''
+  authService = inject(Auth);
+
+  router = inject(Router);
+
+  isLoading: boolean = false;
+
+  formSubmit = signal<boolean>(false);
+
+  senha = signal<string>("");
+
+  email = signal<string>("");
+
+  emailInvalid = computed(() => {
+    return this.email() === "";
+  })
+
+  senhaInvalid = computed(() => {
+    return this.senha() === "";
+  })
+
+  errorMessage = computed(() => {
+    return this.senhaInvalid() || this.emailInvalid()
+      ? "Usuário ou senha inválido"
+      : ""
+  })
+
+  onSubmit() {
+
+    if (!this.emailInvalid() && !this.senhaInvalid()) {
+
+      let result = this.authService.autenticar(
+        this.email(),
+        this.senha()
+      );
+
+      if (result) {
+        this.formSubmit.set(false);
+        this.router.navigate([''])
+      }
+
+      this.email.set("");
+      this.senha.set("");
+
+      this.formSubmit.set(true);
     }
 
-    onSubmit() {
-      console.log('Dados Enviados: ', this.dadosLogin);
-      alert(`Login de ${this.dadosLogin.email} efetivado.`)
-    }
+    this.formSubmit.set(true);
+
+  }
 }
+
+
---
+++ src/app/services/auth/auth.ts
@@ -0,0 +1,46 @@
+import { Injectable, signal } from "@angular/core";
+import { User } from "../../shared/models/User";
+
+@Injectable({
+    providedIn: 'root',
+})
+
+export class Auth {
+    private usuarios: User[] = [
+        { email: 'bla1@gmail.com', senha: '123' },
+        { email: 'bla2@gmail.com', senha: '456' },
+    ]
+
+    private getInitialUser(): User | null {
+        const userJson = localStorage.getItem('auth');
+
+        return userJson ? JSON.parse(userJson) : null;
+    }
+
+    userAuth = signal<User | null>(this.getInitialUser());
+
+    isAuthenticated = signal<boolean>(!!this.getInitialUser());
+
+    autenticar(email: string, senha: string): boolean {
+        let user = this.usuarios.find(
+            (user) => user.email == email
+        )
+
+        if (user) {
+            if (user.senha !== senha) {
+                return false;
+            }
+
+            this.userAuth.set(user)
+
+            localStorage.setItem(
+                'auth',
+                JSON.stringify(user)
+            )
+
+            return true;
+        }
+
+        return false;
+    }
+}
